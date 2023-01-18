face73b157cec24b1d728d5145ec8b871a58f945
wocks <wocks1123@gmail.com>
2023-01-18 15:36:19 +0900

    #1 User 모델 수정
    - 불필요 라인 제거
    - 프로필 이미지 컬럼 추가

--- app/models/User.py
+++ app/models/User.py
@@ -1,16 +1,13 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 
-
-from app.models.ModelBase import Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from app.db.BaseClass import Base
 
 
 class User(Base):
-    # uuid = Column(UUIDType(binary=False), primary_key=True, default=uuid4)
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    userId = Column(String(128), nullable=False)
-    userName = Column(String(128), nullable=False)
-    password = Column(String(64), nullable=False)
-    email = Column(String(128), unique=True, index=True, nullable=False)
+    userId = Column(String(64), index=True, unique=True, nullable=False)
+    userName = Column(String(64), nullable=False)
+    password = Column(String(32), nullable=False)
+    email = Column(String(64), unique=True, nullable=False)
+    profileMessage = Column(String(128), nullable=True)
+    profileImagePath = Column(String(64), nullable=True)
