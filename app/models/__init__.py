c07c8da767665098a13aca9ab08cc48899bc6271
wocks <wocks1123@gmail.com>
2023-02-05 18:02:05 +0900

    불필요 파일 삭제
    - 기존 model, router, schema, crud 구조에서 func/model, router, schame 구조로 변경

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Comment import Comment
-from .CommentGood import CommentGood
-from .MediaFile import MediaFile
-from .Post import Post
-from .PostGood import PostGood
-from .PostMedia import PostMedia
-from .User import User
